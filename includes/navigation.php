1658cfa367d03bf872e24583cefe062d52613079
Lucaswlh <lwlh1103@gmail.com>
2025-12-31 16:38:07 +0800

    updated each role navigation bar

--- includes/navigation.php
+++ includes/navigation.php
@@ -53,24 +53,49 @@ $base_path = '/Group7_EcoQuest/'; // Update this if your project folder is diffe
         
 
         <?php elseif ($user_role == 'moderator'): ?>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/dashboard.php">Dashboard</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/manage_submissions.php">Submissions</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/manage_reports.php">Reports</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/manage_users.php">Users</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/manage_quests.php">Quests</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/moderator/manage_rewards.php">Rewards</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/forum.php">Forum</a></li>
+            <li class="dropdown-wrapper"><a href="<?php echo $base_path; ?>pages/moderator/dashboard.php" class="dropdown-title">Dashboard</a></li>
+            <li class="dropdown-wrapper">
+                <a href="" class="dropdown-title">Manage</a>
+                <ul class="dropdown-content">
+                    <li><a href="<?php echo $base_path; ?>pages/moderator/manage_users.php"><img src="../assets/images/quest.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Users</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/moderator/manage_reports.php"><img src="../assets/images/leaderboard.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Reports</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/moderator/manage_submissions.php"><img src="../assets/images/submission.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Submissions</strong></p></a></li>
+                </ul>
+            </li>
+            <li class="dropdown-wrapper">
+                <a href="" class="dropdown-title">Review</a>
+                <ul class="dropdown-content">
+                    <li><a href="<?php echo $base_path; ?>pages/moderator/manage_quests.php"><img src="../assets/images/reward.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Quests</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/moderator/manage_rewards.php"><img src="../assets/images/claim.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Reward</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/forum.php"><img src="../assets/images/achievement.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Forum</strong></p></a></li>
+                </ul>
+            </li>
 
         <?php elseif ($user_role == 'admin'): ?>
-            <li><a href="<?php echo $base_path; ?>pages/admin/dashboard.php">Dashboard</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/manage_submissions.php">Validate</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/manage_users.php">Users</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/moderation_records.php">Moderation Log</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/manage_quests.php">Quests</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/manage_badges.php">Badges</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/manage_rewards.php">Rewards</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/forum.php">Forum</a></li>
-            <li><a href="<?php echo $base_path; ?>pages/admin/view_feedback.php">Feedback</a></li>
+            <li class="dropdown-wrapper"><a href="<?php echo $base_path; ?>pages/admin/dashboard.php" class="dropdown-title">Dashboard</a></li>
+            <li class="dropdown-wrapper">
+                <a href="" class="dropdown-title">Manage</a>
+                <ul class="dropdown-content">
+                    <li><a href="<?php echo $base_path; ?>pages/admin/manage_users.php"><img src="../assets/images/quest.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Users</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/admin/manage_submissions.php"><img src="../assets/images/leaderboard.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Submissions</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/admin/manage_quests.php"><img src="../assets/images/submission.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Quests</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/admin/manage_badges.php"><img src="../assets/images/submission.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Badge</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/admin/manage_rewards.php"><img src="../assets/images/submission.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Rewards</strong></p></a></li>
+                </ul>
+            </li>
+            <li class="dropdown-wrapper">
+                <a href="" class="dropdown-title">Moderate</a>
+                <ul class="dropdown-content">
+                    <li><a href="<?php echo $base_path; ?>pages/admin/moderation_records.php"><img src="../assets/images/reward.png" alt="reward image" class="dropdown-img"><p><strong class="dropdown-title-text">Moderation Log</strong></p></a></li>
+                </ul>
+            </li>
+            <li class="dropdown-wrapper">
+                <a href="" class="dropdown-title">Review</a>
+                <ul class="dropdown-content">
+                    <li><a href="<?php echo $base_path; ?>pages/admin/view_feedback.php"><img src="../assets/images/reward.png" alt="reward image" class="dropdown-img"><p><strong class="dropdown-title-text">Feedback</strong></p></a></li>
+                    <li><a href="<?php echo $base_path; ?>pages/forum.php"><img src="../assets/images/achievement.png" alt="" class="dropdown-img"><p><strong class="dropdown-title-text">Forum</strong></p></a></li>
+                </ul>
+            </li>
         <?php endif; ?>
     </div>
 
